2215794dd813ea9e0560702d2e49a495f8e626c2
Charith Mendis <charithm@csail.mit.edu>
2014-10-02 00:47:01 +0530

    code_cov updated

--- preprocess/code_cov/code_cov.bat
+++ preprocess/code_cov/code_cov.bat
@@ -4,6 +4,7 @@ set LOGDIR=%EXALGO_OUTPUT_FOLDER%
 set DREXPORT=%DYNAMORIO_HOME%\exports
 set PHOTOSHOP32="C:\Program Files (x86)\Adobe\Adobe Photoshop CS6\Photoshop.exe"
 set PHOTOSHOP64="C:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe"
+set GIMP="C:\Program Files\GIMP 2\bin\gimp-2.8.exe"
 
 if "%1"=="m32" (
 %DREXPORT%\bin32\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP32%
@@ -11,4 +12,8 @@ if "%1"=="m32" (
 
 if "%1"=="m64" (
 %DREXPORT%\bin64\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP64%
+)
+
+if "%1" NEQ "m32" (
+%DREXPORT%\bin64\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %1%
 )
