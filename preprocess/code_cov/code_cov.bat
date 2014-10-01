f19d4a76a0850bf52beeaf5261d8caf790fbd1a7
Charith Mendis <charithm@csail.mit.edu>
2014-10-02 01:12:36 +0530

    run file updated again 2

--- preprocess/code_cov/code_cov.bat
+++ preprocess/code_cov/code_cov.bat
@@ -6,16 +6,25 @@ set PHOTOSHOP32="C:\Program Files (x86)\Adobe\Adobe Photoshop CS6\Photoshop.exe"
 set PHOTOSHOP64="C:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe"
 set GIMP="C:\Program Files\GIMP 2\bin\gimp-2.8.exe"
 
+
+
 if "%1"=="m32" (
-%DREXPORT%\bin32\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP32%
+ if "%2"=="photoshop" (
+	%DREXPORT%\bin32\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP32%
+ )
+ if "%2"=="gimp" (
+	%DREXPORT%\bin32\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %GIMP%
+ )
 )
 
 if "%1"=="m64" (
-%DREXPORT%\bin64\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP64%
+ if "%2"=="photoshop" (
+	%DREXPORT%\bin64\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %PHOTOSHOP64%
+ )
+ if "%2"=="gimp" (
+	%DREXPORT%\bin64\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %GIMP%
+ )
 )
 
-if "%1"=="gimp" (
-%DREXPORT%\bin32\drrun.exe -root %DREXPORT% -t drcov -dump_text -logdir %LOGDIR% -- %GIMP%
-)
 
 
